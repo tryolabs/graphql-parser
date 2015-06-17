0c8bb6d9db9d1b233ea46851706bb427de8b6a0b
Fernando Borretti <eudoxiahp@gmail.com>
2015-06-17 17:18:36 -0300

    More classes

--- graphql_parser/parser.py
+++ graphql_parser/parser.py
@@ -15,6 +15,15 @@ class Field(Node):
     """A field name in a query."""
     grammar = name()
 
+class Value(object):
+    pass
+
+class Id(object):
+    grammar = name(), ":", Value
+
+class Function(object):
+    grammar = name(), "(", Id, ")"
+
 class Block(object):
     """A curly brace delimited block."""
     grammar = "{", csl(Node), "}"
