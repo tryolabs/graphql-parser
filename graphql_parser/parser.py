94c97d4c53bb61b26d04afa1dd5986b1f1287d2a
Fernando Borretti <eudoxiahp@gmail.com>
2015-06-17 17:06:03 -0300

    Add some classes for the parser

--- graphql_parser/parser.py
+++ graphql_parser/parser.py
@@ -6,3 +6,15 @@ This module implements the parser.
 :license:   MIT
 """
 
+from pypeg2 import name
+
+class Node(object):
+    """Base class of node objects."""
+
+class Field(Node):
+    """A field name in a query."""
+    grammar = name()
+
+class Block(object):
+    """A curly brace delimited block."""
+    grammar = "{", csl(Node), "}"
