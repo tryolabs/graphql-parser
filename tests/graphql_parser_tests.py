b12490b6fafbf283a2991062824518873d816165
Fernando Borretti <eudoxiahp@gmail.com>
2015-06-18 12:50:24 -0300

    Some tests

--- tests/graphql_parser_tests.py
+++ tests/graphql_parser_tests.py
@@ -2,9 +2,12 @@
 from nose.tools import *
 from pypeg2 import parse
 
-from graphql_parser.parser import number
+from graphql_parser.parser import number, Arguments
 
-def test_arguments():
+def test_number():
     assert parse("1", number) == '1'
     assert parse("1234", number) == '1234'
     assert parse("-34", number) == '-34'
+
+def test_arguments():
+    assert parse("1,2,3", Arguments) == ['1', '2', '3']
