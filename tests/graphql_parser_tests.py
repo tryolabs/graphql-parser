402e046cbafb7a858277456da273da6666e21c6d
Fernando Borretti <eudoxiahp@gmail.com>
2015-06-18 17:13:01 -0300

    Test function calls

--- tests/graphql_parser_tests.py
+++ tests/graphql_parser_tests.py
@@ -2,7 +2,7 @@
 from nose.tools import *
 from pypeg2 import parse
 
-from graphql_parser.parser import number, Arguments, Field, Block
+from graphql_parser.parser import number, Arguments, Field, Block, Call
 
 def test_number():
     assert parse('1', number) == '1'
@@ -18,3 +18,14 @@ def test_field():
 
 def test_block():
     assert [f.name for f in parse('{a,b,c}', Block)] == ['a', 'b', 'c']
+
+def test_call():
+    call = parse('test(3, 4) { a, b, c }', Call)
+    assert [f.name for f in call.names()] == ['test']
+    assert call.arguments() == ['3', '4']
+    assert [f.name for f in call.body()] == ['a', 'b', 'c']
+
+    call = parse('test.method(1) { a }', Call)
+    assert [f.name for f in call.names()] == ['test', 'method']
+    assert call.arguments() == ['1']
+    assert [f.name for f in call.body()] == ['a']
