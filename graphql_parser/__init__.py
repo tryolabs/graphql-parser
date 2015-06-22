97029866c96dd6bc46b76d952a28017d105c12db
Fernando Borretti <eudoxiahp@gmail.com>
2015-06-22 17:20:09 -0300

    Minor fix

--- graphql_parser/__init__.py
+++ graphql_parser/__init__.py
@@ -1,6 +1,8 @@
+import pypeg2
+
 from graphql_parser.parser import Block
 from graphql_parser.transform import transform_block
-from pypeg2 import parse
+
 
 def parse(string):
-    transform_block(parse(string, Block))
+    transform_block(pypeg2.parse(string, Block))
