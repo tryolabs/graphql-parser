f8def567ac3244f942d169437263735b70661325
Fernando Borretti <eudoxiahp@gmail.com>
2015-06-22 17:19:50 -0300

    Add some missing whitespace

--- graphql_parser/parser.py
+++ graphql_parser/parser.py
@@ -9,18 +9,23 @@ This module implements the parser.
 import re
 from pypeg2 import name, csl, List
 
+
 class Field():
     """A field name in a query."""
     grammar = name()
 
+
 number = re.compile(r'[+-]?(\d)+')
+
 
 class Arguments(List):
     """Arguments to a call."""
     grammar = csl(number, separator=',')
 
+
 class CallList(List):
     grammar = csl(Field, separator='.')
+
 
 class Call(List):
     """A function call."""
@@ -34,8 +39,10 @@ class Call(List):
     def body(self):
         return self[2]
 
+
 class Block(List):
     """A curly brace delimited block."""
     grammar = '{', csl([Call, Field], separator=','), '}'
+
 
 Call.grammar = CallList, '(', Arguments, ')', Block
